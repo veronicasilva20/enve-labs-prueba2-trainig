51172eff794cbfbba0537b34791a128385b972f8
veronicasilva20 <santinoveronicasilva@gmail.com>
2023-07-31 12:27:54 -0300

    agregar texto

---
+++ main.js
@@ -0,0 +1,91 @@
+// // let frutas= ["banana", "naranja"]
+// // let fruta2=[]
+
+// // console.log(frutas[2])
+
+let numeros =['2','8','1'];
+let numeroDuplicado=numeros.map(function(numero){
+    return numero*2
+})
+console.log (numeroDuplicado)
+ 
+// // let numero=[3,5,1,9]
+// // if (numero >=9){
+// //     console.log('si es mayor');
+// // }else {
+// //     console.log('eres menos ')
+// // }
+
+// // function frutas
+
+
+
+// // let frutas = ['banana','naranja'];
+// // let frutasSeleccionada = frutas.slice(2);
+// // console.log(frutas);
+
+
+// let nombre=["hola como esta"];
+// console.log(nombres);
+// for (let nombre = 0;i < array.length;nombre++) {
+//     let = arraynombre;
+//     return 
+// }
+
+
+
+
+
+
+// function pedirNumeroUsuario() {
+//   let numero;
+//   while (true) {
+//     let input = prompt("Ingrese un número:");
+//     numero = Number(input);
+//     if (!isNaN(numero)) {
+//       break;
+//     }
+//     alert("ingrese un valor ");
+//   }
+//   return numero;
+// }
+// let numeroIngresado = pedirNumeroUsuario();
+// console.log("Número ingresado:", numeroIngresado);
+
+
+// let edad = 65;
+// if (edad>=65){
+//     console.log("sos de la tercera edad");
+// }else {
+//     console.log("sos menor de edad");
+// }
+// if (edad>=18){
+//     console.log("sos mayorde edad");
+// }
+// function imprimirSaludo() {
+//     for (let i = 0; i < 15; i++) {
+//       console.log("Hola, ¿cómo estás?");
+//     }}
+
+//   imprimirSaludo();
+  
+//   function imprimirSaludo() {
+//     for (let i = 0; i < 15; i++) {
+//       console.log("Hola, ¿cómo estás?");
+//     }
+//   }
+//   imprimirSaludo();
+  
+
+
+      
+    
+
+//       function existeFruta(palabra, array) {
+//         return array.includes(palabra);
+//       }
+      
+//       const frutas = ["manzana", "plátano", "naranja", "pera"];
+//       console.log(existeFruta("manzana", frutas)); 
+//       console.log(existeFruta("kiwi", frutas)); 
+      
